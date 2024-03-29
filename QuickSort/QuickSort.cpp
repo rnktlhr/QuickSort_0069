7c32acbc28f8bc5d62e27596cdb7eae1d73e33b1
rnktlhr <rinakitluhur10@gmail.com>
2024-03-29 14:03:10 +0700

    menambahkan do while pada int main

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -86,4 +86,12 @@ void display() {
 int main()
 {
     char ch;
+
+    do {
+        input();
+        q_short(0, n-1);
+        display();
+        cout << "\n\nDo you want to continue? (y/n)";
+        cin >> ch;
+    } while (true);
 }
