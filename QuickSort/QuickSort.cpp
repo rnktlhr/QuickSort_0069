86ed95340a7217d2ac67d0aa0ac25533040d4542
rnktlhr <rinakitluhur10@gmail.com>
2024-03-29 13:31:31 +0700

    menambahkan for pada void input

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -17,6 +17,15 @@ void input() {
         else
             cout << "\nMaksimum panjang array adalah 20" << endl;
     }
+
+    cout << "\n-------------------" << endl;
+    cout << "\nEnter Array Element" << endl;
+    cout << "\n-------------------" << endl;
+
+    for (int i = 0; i < n; i++) {
+        cout << "<" << (1 + 1) << ">";
+        cin >> arr[i];
+    }
 }
 
 int main()
